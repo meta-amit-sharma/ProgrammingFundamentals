9f0b3ec0ffa841af6b378368bac73bf747a62170
meta-amit-sharma <35561713+meta-amit-sharma@users.noreply.github.com>
2018-01-30 17:38:30 +0530

    Spaces and indentation updated

--- Assignment3PF/LargestDigit/LargestDigit.java
+++ Assignment3PF/LargestDigit/LargestDigit.java
@@ -2,14 +2,12 @@
  * This file contains the class named LargestDigit
  */
 package Session3.LargestDigit;
-
 /**
  * This class contains the function that finds the largest digit in a number
  * @author Amit Sharma
- *
+ * @version 1.0
  */
 public class LargestDigit {
-	
 	/**
 	 * This function takes a number
 	 * and returns the larger number between 
@@ -20,15 +18,13 @@ public class LargestDigit {
 	 * If the function has the single digit input,it returns the same value
 	 * @param x input number
 	 */
-	public int largestdigit (int x) {
-		
+	public int largestdigit ( int x ) {
 		/*This is the base condition for the recursion*/
-		if( (x / 10) == 0) {
+		if( ( x / 10 ) == 0) {
 			return x;
 		}
-		
 		/*This returns the maximum of the last and the second last digit*/
-		return Math.max( (x % 10), largestdigit(x/10) );
+		return Math.max( ( x % 10 ), largestdigit( x / 10 ) );
 	}
 
 }
