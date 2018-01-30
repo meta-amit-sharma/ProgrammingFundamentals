12a753c324299cede5493093938c0cb4b74b31fa
meta-amit-sharma <35561713+meta-amit-sharma@users.noreply.github.com>
2018-01-30 17:44:40 +0530

    Spaces and indentation updated

--- Assignment3PF/QuickSort/QuickSortArray.java
+++ Assignment3PF/QuickSort/QuickSortArray.java
@@ -2,15 +2,13 @@
  * This file contains the class named QuickSort
  */
 package Session3.QuickSort;
-
 /**
  * This class contains two method one for partitioning the array
  * and other for performing quicksort
  * @author Amit Sharma
- *
+ * @version 1.0
  */
 public class QuickSortArray {
-	
 	/**
 	 * This function takes the array to be sorted, and performs sorting 
 	 * @param inputArray
@@ -18,21 +16,17 @@ public class QuickSortArray {
 	 * @param endIndex
 	 * @return returns the final array
 	 */
-	public int[] quicksort ( int inputArray[], int startIndex, int endIndex) {
+	public int[] quicksort ( int inputArray[], int startIndex, int endIndex ) {
 		if ( startIndex < endIndex ) {
-			
 			/*to find the position from where the array needs to be partitioned*/
-			int pivot = partition ( inputArray, startIndex, endIndex);
-			
+			int pivot = partition ( inputArray, startIndex, endIndex );
 			/*for the first half of the array*/
-			quicksort ( inputArray, startIndex, pivot - 1);
-			
+			quicksort ( inputArray, startIndex, pivot - 1 );
 			/*for the second half of the array*/
-			quicksort ( inputArray, pivot + 1, endIndex);
+			quicksort ( inputArray, pivot + 1, endIndex );
 		}
 		return inputArray;
 	}
-	
 	/**
 	 * This function returns the position from where the array needs to be partitioned
 	 * and also places the pivot element in its correct place
@@ -41,12 +35,10 @@ public class QuickSortArray {
 	 * @param high
 	 * @return returns the position of pivot element
 	 */
-	public int partition ( int arr[], int low, int high) {
+	public int partition ( int arr[], int low, int high ) {
 		int i = low - 1;
-		
 		/*taking pivot to be the last element of array */
 		int pivot = arr[ high ];
-		
 		/* Swapping is performed here to bring all the elements which are less than
 		 * the pivot element to the left side, while all the elements greater than 
 		 * the pivot elements to the right side
@@ -59,12 +51,11 @@ public class QuickSortArray {
 				arr[ i ] = temp;
 			}
 		}
-		
 		/* here the pivot element is brought in its right location*/
-		int temp = arr[ i + 1];
-		arr[ i + 1] = arr[ high ];
+		int temp = arr[ i + 1 ];
+		arr[ i + 1 ] = arr[ high ];
 		arr[ high ] = temp;
-		return ( i + 1 ); //position of the pivot element
+		return ( i + 1 ); 			//position of the pivot element
 	}
 
 }
