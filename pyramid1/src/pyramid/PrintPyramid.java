e8bdaee37cf02570c25c9f02886e26027bd6d834
meta-amit-sharma <35561713+meta-amit-sharma@users.noreply.github.com>
2018-01-30 17:25:28 +0530

    spaces and indentations corrected

--- pyramid1/src/pyramid/PrintPyramid.java
+++ pyramid1/src/pyramid/PrintPyramid.java
@@ -8,10 +8,9 @@
  * one method returns the string containing numbers
  * last method returns the string containing final pyramid
  * @author Amit Kumar Sharma
- *
+ * @version 1.0
  */
 public class PrintPyramid {
-
 	/**
 	 * This function returns String of spaces
 	 * @param row
@@ -25,8 +24,6 @@ public class PrintPyramid {
 		}
 		return s;
 	}
-	
-	
 	/**
 	 * This function returns String of numbers
 	 * @param row
@@ -35,21 +32,17 @@ public class PrintPyramid {
 	 */
 	String numbers (int row, int n) {
 		String s = "";
-		int i;							//counter for loops
-		
+		int i;								//counter for loops
 		/*This loop is for the numbers in increasing order*/
 		for (i = 1; i <= row; i++) {
 			s += i;
 		}
-		
 		/*This loop is for the numbers in decreasing order*/
 		for (int j = i - 2; j >= 1; j--) {
 			s += j;
 		}
 		return s;
 	}
-	
-	
 	/**
 	 * This function returns String of final pyramid
 	 * @param n
@@ -57,13 +50,11 @@ public class PrintPyramid {
 	 */
 	String[] print (int n) {
 		String s[] = new String[ 2 * n - 1];
-		
 		/*This loop prints the upper half of the pattern*/
 		for (int i = 1; i <= n; i++) {
-			s[i - 1] = ( spaces(i, n) + numbers(i, n) ); 	//concatenates space and number string 
+			s[i - 1] = ( spaces(i, n) + numbers(i, n) ); 		//concatenates space and number string 
 			System.out.println(s[i - 1]);
 		}
-		
 		/*This loop prints the lower half of the pattern*/
 		for (int i = 4; i >= 1; i--) {
 			s[(2 * n - 1) - i] = ( spaces(i, n) + numbers(i, n) );	//concatenates space and number string 
@@ -71,5 +62,4 @@ public class PrintPyramid {
 		}
 		return s;
 	}
-	
 }
