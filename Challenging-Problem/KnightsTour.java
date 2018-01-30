9918987aaa9d9cde75dd8f4ce727c01fb904f875
meta-amit-sharma <35561713+meta-amit-sharma@users.noreply.github.com>
2018-01-30 23:33:09 +0530

    Updated spaces and indentation

--- Challenging-Problem/KnightsTour.java
+++ Challenging-Problem/KnightsTour.java
@@ -9,7 +9,6 @@ package Session4.KnightsTour;
  * @version 1.0
  */
 public class KnightsTour {
-	
 	/**
 	 * This method return the whether knight completed its tour.
 	 * @param board - chess board of nXn size
@@ -20,7 +19,6 @@ public class KnightsTour {
 		board[0][0] = 1;
 		return (knightsVisit (board, dimension, 0, 0, 1)) ;
 	}
-	
 	/**
 	 * This method check if the square is safe for knight to visit 
 	 * @param board
@@ -30,10 +28,8 @@ public class KnightsTour {
 	 * @return either true or false
 	 */
 	static boolean isSafe ( int board[][], int dimension, int rowNum, int colNum) {
-		return ( (rowNum >= 0) && (rowNum < dimension) && 
-					(colNum >=0) && (colNum < dimension) && (board[rowNum][colNum] == 0) );
+		return ( (rowNum >= 0) && (rowNum < dimension) && (colNum >=0) && (colNum < dimension) && (board[rowNum][colNum] == 0) );
 	}
-	
 	/**
 	 * This method implements the algorithm.
 	 * @param board
@@ -43,14 +39,11 @@ public class KnightsTour {
 	 * @param count - keeps the count of squares visited in total
 	 */
 	static boolean knightsVisit ( int board[][], int dimension, int rowNum, int colNum, int count) {
-		
 		/*if all squares are visited return true*/
 		if( count == dimension * dimension) {
 			return true;
 		}
-		
 		/*check for all possible 8 cases to move*/
-		
 		/*First case*/
 		if ( isSafe (board, dimension, rowNum + 2, colNum + 1) ) {
 			board[rowNum + 2][colNum + 1] = count + 1;		//if safe then make a visit
@@ -60,7 +53,6 @@ public class KnightsTour {
 			}
 			board[rowNum + 2][colNum + 1] = 0;				//not safe backtrack
 		}
-		
 		/*Second case*/
 		if ( isSafe (board, dimension, rowNum + 1, colNum + 2) ) {
 			board[rowNum + 1][colNum + 2] = count + 1;		//if safe then make a visit
@@ -70,7 +62,6 @@ public class KnightsTour {
 			}
 			board[rowNum + 1][colNum + 2] = 0;				//not safe backtrack
 		}
-		
 		/*Third case*/
 		if ( isSafe (board, dimension, rowNum - 1, colNum + 2) ) {
 			board[rowNum - 1][colNum + 2] = count + 1;		//if safe then make a visit
@@ -80,7 +71,6 @@ public class KnightsTour {
 			}
 			board[rowNum - 1][colNum + 2] = 0;				//not safe backtrack
 		}
-		
 		/*Fourth case*/
 		if ( isSafe (board, dimension, rowNum - 2, colNum + 1) ) {
 			board[rowNum - 2][colNum + 1] = count + 1;		//if safe then make a visit
@@ -90,7 +80,6 @@ public class KnightsTour {
 			}
 			board[rowNum - 2][colNum + 1] = 0;				//not safe backtrack
 		}
-		
 		/*Fifth case*/
 		if ( isSafe (board, dimension, rowNum - 2, colNum - 1) ) {
 			board[rowNum - 2][colNum - 1] = count + 1;		//if safe then make a visit
@@ -100,7 +89,6 @@ public class KnightsTour {
 			}
 			board[rowNum - 2][colNum - 1] = 0;				//not safe backtrack
 		}
-		
 		/*Sixth case*/
 		if ( isSafe (board, dimension, rowNum - 1, colNum - 2) ) {
 			board[rowNum - 1][colNum - 2] = count + 1;		//if safe then make a visit
@@ -110,7 +98,6 @@ public class KnightsTour {
 			}
 			board[rowNum - 1][colNum - 2] = 0;				//not safe backtrack
 		}
-		
 		/*Seventh case*/
 		if ( isSafe (board, dimension, rowNum + 1, colNum - 2) ) {
 			board[rowNum + 1][colNum - 2] = count + 1;		//if safe then make a visit
@@ -120,7 +107,6 @@ public class KnightsTour {
 			}
 			board[rowNum + 1][colNum - 2] = 0;				//not safe backtrack
 		}
-		
 		/*Eighth case*/
 		if ( isSafe (board, dimension, rowNum + 2, colNum - 1) ) {
 			board[rowNum + 2][colNum - 1] = count + 1;		//if safe then make a visit
@@ -132,17 +118,16 @@ public class KnightsTour {
 		}
 		return false;			//return false or backtrack if no case is safe to visit
 	}
-	
 	/**
-	 * driver function to test the above implementation
+	 * main method to test the above implementation
 	 */
 	public static void main(String args[]) {
 		int dimension = 8;
 		int[][] board = new int[dimension][dimension];
 		knightsTour(board, dimension);
-		for (int i = 0; i < dimension; i++) {
-			for (int j = 0;j < dimension; j++) {
-				System.out.print( board[i][j] + "  ");
+		for (int row = 0; row < dimension; row++) {
+			for (int column = 0;column < dimension; column++) {
+				System.out.print( board[row][column] + "  ");
 			}
 			System.out.println();
 		}
