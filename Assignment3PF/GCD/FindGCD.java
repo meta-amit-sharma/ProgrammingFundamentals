1334cd13eca804b5f1540326bdfd2faaf90e041a
meta-amit-sharma <35561713+meta-amit-sharma@users.noreply.github.com>
2018-01-30 17:36:17 +0530

    Spaces and indentation updated

--- Assignment3PF/GCD/FindGCD.java
+++ Assignment3PF/GCD/FindGCD.java
@@ -2,16 +2,14 @@
  * This file contains the class named FindGCD
  */
 package Session3.GCD;
-
 /**
  * This class contains the function 
  * Function takes two numbers as input 
  * and returns the GCD of the two numbers
  * @author Amit Sharma
- *
+ * @version 1.0
  */
 public class FindGCD {
-	
 	/**
 	 * This returns the GCD of two numbers
 	 * @param x First number
@@ -19,24 +17,18 @@ public class FindGCD {
 	 * @return  GCD of the two number
 	 */
 	public int gcd (int x, int y) {
-		
 		if ( y == 0 ) {
-			
 			/*If y is zero it returns the non-zero number x*/
 			if ( x != 0) {
 				return x;
 			}
-			
 			/*If  both the numbers are zero then the GCD is Undefined*/ 
 			throw new ArithmeticException("Undefined Value");
 		}
-		
 		/*If y completely divides x, then y is the GCD*/
-		if ( ( x % y) == 0 ) {
+		if ( ( x % y ) == 0 ) {
 			return y;
 		}
-		
 		return gcd ( y, (x % y) );	
 	}
-	
 }
