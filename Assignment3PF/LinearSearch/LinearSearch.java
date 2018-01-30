b756d2a61d20f4218228279cdff20dcd34f879c9
meta-amit-sharma <35561713+meta-amit-sharma@users.noreply.github.com>
2018-01-30 17:40:39 +0530

    Spaces and indentation updated

--- Assignment3PF/LinearSearch/LinearSearch.java
+++ Assignment3PF/LinearSearch/LinearSearch.java
@@ -2,16 +2,14 @@
  * This file contains the class named LinearSearch
  */
 package Session3.LinearSearch;
-
 /**
  * This class contains the recursive function
  * to find if the given number is present in the array
  * @author Amit Sharma
- *
+ * @version 1.0
  */
 public class LinearSearch {
-    
-	/**
+    	/**
 	 * This function takes the search term and the array as input
 	 * and search using Linear search. It returns true if the number exists 
 	 * in the array or else false
@@ -21,16 +19,14 @@ public class LinearSearch {
 	 * @return final results
 	 */
 	public boolean linearSearch (int n, int inputArray[], int a ) {
-		
 		/*To check if we reach till the end of the Array*/
 		if( a >= inputArray.length ) {
 			return false;
 		} 
-		
 		/*If we find that the given number is present in the Array*/
 		if ( inputArray[ a ] == n ) {
 			return true;
 		}
-		return linearSearch ( n, inputArray, a+1);
+		return linearSearch ( n, inputArray, a + 1 );
 	}
 }
