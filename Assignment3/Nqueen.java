b8a56551ca627a60f9e6ff61dd2a005a0ee0a835
meta-amit-sharma <35561713+meta-amit-sharma@users.noreply.github.com>
2018-01-30 23:28:30 +0530

    variable names and indentation updated

--- Assignment3/Nqueen.java
+++ Assignment3/Nqueen.java
@@ -2,15 +2,13 @@
  * This file contains the class named Nqueen 
  */
 package Session4.NQueen;
-
 /**
  * This class gives the solution to N-Queen problem
  * @author Amit Sharma
  * @version 1.0
  */
 public class Nqueen {
-    
-	/**
+        /**
 	 * This function takes the board as input
 	 * it then recursively determines the solution 
 	 * to the N-Queen problem
@@ -24,18 +22,17 @@ public class Nqueen {
 		if ( startRow >= dimensionOfMatrix){
 			return true;
 		}
-			for ( int column = 0 ; column < dimensionOfMatrix ; column++ ) {
-				
-				if ( isSafe( board, startRow, column, dimensionOfMatrix) ) {
-					board[ startRow ][ column ] = 1 ; //storing 1 in the current column indicates that queen is placed in that square
-					if ( nQueen ( board, startRow + 1, dimensionOfMatrix) ) {
-						return true; //returns true if the queen can be safely placed
-					}
-					board[ startRow ][ column ] = 0 ; //stores 0 again if the current position of queen is not safe during backtracking
+		for ( int column = 0 ; column < dimensionOfMatrix ; column++ ) {
+			if ( isSafe( board, startRow, column, dimensionOfMatrix) ) {
+				board[ startRow ][ column ] = 1 ; 					//storing 1 in the current column indicates that queen is placed in that square
+				if ( nQueen ( board, startRow + 1, dimensionOfMatrix) ) {
+					return true; 							//returns true if the queen can be safely placed
 				}
+				board[ startRow ][ column ] = 0 ; 					//stores 0 again if the current position of queen is not safe during backtracking
 			}
+		}
 		
-		return false; // returns false if the queen is not safe in the current position
+		return false; 										// returns false if the queen is not safe in the current position
 	}
 	
 	/**
@@ -43,62 +40,62 @@ public class Nqueen {
 	 * @param board
 	 * @param row
 	 * @param column
-	 * @param n
+	 * @param sizeOfBoard
 	 * @return true if the position is safe for the queen
 	 */
-	boolean isSafe( int board[][], int row, int column, int n ) {
-		if ( checkRow (board, row, n) ) {									//check for another queen in given row
-			if ( checkColumn (board, column, n) ) {							//check for another queen in given column
-				if ( checkLeftDiagonal (board, row, column, n) ) {			//check for another queen in left diagonal
-					if ( checkRightDiagonal (board, row, column, n) ) { 	//check for another queen in right diagonal
+	boolean isSafe( int board[][], int row, int column, int sizeOfBoard ) {
+		if ( checkRow (board, row, sizeOfBoard) ) {							//check for another queen in given row
+			if ( checkColumn (board, column, sizeOfBoard) ) {					//check for another queen in given column
+				if ( checkLeftDiagonal (board, row, column, sizeOfBoard) ) {			//check for another queen in left diagonal
+					if ( checkRightDiagonal (board, row, column, sizeOfBoard) ) { 		//check for another queen in right diagonal
 						return true;
 					}
 				} 
 			}
 		}
-		return false;														//returns false if its not safe for the placement of queen
+		return false;											//returns false if its not safe for the placement of queen
 	}
 	
-	boolean checkRow (int board[][], int row, int n ) {
-		for(int i = 0; i < n; i++) {
-			if (board[row][i] == 1) {
+	boolean checkRow (int board[][], int row, int sizeOfBoard ) {
+		for(int column = 0; column < sizeOfBoard; column++) {
+			if (board[row][column] == 1) {
 				return false;
 			}
 		}
 		return true; 
 	}
 	
-	boolean checkColumn (int board[][], int column, int n ) {
-		for(int i = 0; i < n; i++) {
-			if (board[i][column] == 1) {
+	boolean checkColumn (int board[][], int column, int sizeOfBoard ) {
+		for(int row = 0; row < sizeOfBoard; row++) {
+			if (board[row][column] == 1) {
 				return false;
 			}
 		}
 		return true;
 	}
 	
-	boolean checkLeftDiagonal (int board[][], int row, int column, int n) {
-		for (int i = row, j = column; (i >= 0) && (j >= 0); i--, j--) {
-			if ( board[ i ][ j ] == 1) {
+	boolean checkLeftDiagonal (int board[][], int row, int column, int sizeOfBoard) {
+		for (int rowIndex = row, columnIndex = column; (rowIndex >= 0) && (columnIndex >= 0); rowIndex--, columnIndex--) {
+			if ( board[ rowIndex ][ columnIndex ] == 1) {
 				return false;
 			}
 		}
-		for (int i = row, j = column; (i < n) && (j < n); i++, j++) {
-			if ( board[ i ][ j ] == 1) {
+		for (int rowIndex = row, columnIndex = column; (rowIndex < sizeOfBoard) && (columnIndex < sizeOfBoard); rowIndex++, columnIndex++) {
+			if ( board[ rowIndex ][ columnIndex ] == 1) {
 				return false;
 			}
 		}
 		return true;
 	}
 	
-	boolean checkRightDiagonal (int board[][], int row, int column, int n) {
-		for (int i = row, j = column; (i >= 0) && (j < n); i--, j++) {
-			if ( board[ i ][ j ] == 1) {
+	boolean checkRightDiagonal (int board[][], int row, int column, int sizeOfBoard) {
+		for (int rowIndex = row, columnIndex = column; (rowIndex >= 0) && (columnIndex < sizeOfBoard); rowIndex--, columnIndex++) {
+			if ( board[ rowIndex ][ columnIndex ] == 1) {
 				return false;
 			}
 		}
-		for (int i = row, j = column; (i < n) && (j >= 0); i++, j--) {
-			if ( board[ i ][ j ] == 1) {
+		for (int rowIndex = row, columnIndex = column; (rowIndex < sizeOfBoard) && (columnIndex >= 0); rowIndex++, columnIndex--) {
+			if ( board[ rowIndex ][ columnIndex ] == 1) {
 				return false;
 			}
 		}
